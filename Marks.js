a041cccbd06c3cd77a006e54242ec3ddf824df9e
AnjliRajput3007 <anjliraj910@gmail.com>
2025-07-31 13:57:00 +0530

    Add marks.js

---
+++ Marks.js
@@ -0,0 +1,15 @@
+
+function marks(num){
+  if(num >= 85){
+    console.log("You passed exam with 1st Rank");
+  } else if(num >= 65){
+    console.log("You passed exam with 2nd Rank");
+  } else if(num >= 36){
+    console.log("You passed exam with 3rd Rank");
+  } else {
+    console.log("You failed the exam");
+  }
+}
+marks(35);
+
+
